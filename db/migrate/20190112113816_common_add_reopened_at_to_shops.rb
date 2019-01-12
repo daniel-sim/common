6c1017f033d01971655846a0463e9386ccb956c3
Jamie Schembri <jamie@pluginuseful.com>
2019-01-12 15:21:54 +0100

    Begin namespacing migrations

--- db/migrate/20190112113816_common_add_reopened_at_to_shops.rb
+++ db/migrate/20190112113816_common_add_reopened_at_to_shops.rb
@@ -1,4 +1,4 @@
-class AddReopenedAtToShops < ActiveRecord::Migration[5.0]
+class CommonAddReopenedAtToShops < ActiveRecord::Migration[5.0]
   def change
     add_column :shops, :reopened_at, :timestamp
   end
