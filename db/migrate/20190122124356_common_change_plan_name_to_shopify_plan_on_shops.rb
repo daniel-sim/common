027c9526baa8f30c424a31716878b173e8af59de
Jamie Schembri <jamie@pluginuseful.com>
2019-01-22 14:28:15 +0100

    Fix up migrations

--- db/migrate/20190122124356_common_change_plan_name_to_shopify_plan_on_shops.rb
+++ db/migrate/20190122124356_common_change_plan_name_to_shopify_plan_on_shops.rb
@@ -1,4 +1,4 @@
-class CommonChangePlanNameToShopifyPlanOnShops < ActiveRecord::Migration[5.2]
+class CommonChangePlanNameToShopifyPlanOnShops < ActiveRecord::Migration[5.0]
   def change
     rename_column :shops, :plan_name, :shopify_plan
   end
