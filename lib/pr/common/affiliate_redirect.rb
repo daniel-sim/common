020285bc8d03a4467d297772c52f0ce6679d0505
Jamie Schembri <jamie@pluginuseful.com>
2019-05-21 12:40:31 +0200

    Add promo codes when using `ref=`

--- lib/pr/common/affiliate_redirect.rb
+++ lib/pr/common/affiliate_redirect.rb
@@ -4,15 +4,44 @@ module PR
       def initialize(app)
         @app = app
       end
-      def call(env)
-        req = Rack::Request.new(env)
-        status, headers, body = @app.call(env)
 
-        if req.env['rack.request.query_hash']['ref'] && PR::Common.config.referrer_redirect
-          return [302, {'Location' => PR::Common.config.referrer_redirect, 'Content-Type' => 'text/html'}, ['Found']]
-        end
+      def call(env)
+        status, headers, body = @app.call(env)
+        @request = Rack::Request.new(env)
+
+        return handle_referral if referrer?
 
         [status, headers, body]
+      end
+
+      private
+
+      def referrer?
+        @request.env['rack.request.query_hash']['ref'] && PR::Common.config.referrer_redirect
+      end
+
+      def handle_referral
+        handle_promo_code
+
+        [
+          302,
+          {'Location' => PR::Common.config.referrer_redirect, 'Content-Type' => 'text/html'},
+          ['Found']
+        ]
+      end
+
+      def handle_promo_code
+        service = SessionPromoCodeService.new(@request.session)
+
+        service.clear
+
+        return if (code = @request.params["promo_code"]).blank?
+
+        promo_code = PR::Common::Models::PromoCode.find_by(code: code)
+
+        return if promo_code.blank?
+
+        service.store(promo_code)
       end
     end
   end
